0f6a71d11d51dd177848bffb4a874becf3b33aee
Rodrigo Maturano Zanetti <rodrigogitzanetti@outlook.com>
2025-09-15 06:04:20 -0300

    commit

--- ex046_Game.py
+++ ex046_Game.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 print('|=|'*20)
 escolha=int(input('''What's your choice?: 
@@ -22,15 +23,18 @@ else:
     print('Sua escolha invalida!')
     raise SystemExit
 
+print('🖥️ Computer is thinking')
+sleep(2)
+
 computer_choice=random.choice(['Rock','Paper','Scissors'])
 print(f'The computer choice: {computer_choice}')
 
 if computer_choice == escolha:
-    print('\033[1;30;43mIt is a tie!\033[m')
+    print('🤝\033[1;30;43mIt is a tie!\033[m')
 elif (computer_choice == 'Rock' and escolha == 'Paper') or (computer_choice == 'Paper' and escolha == 'Scissors') or (computer_choice == 'Scissors' and escolha == 'Rock'):
-    print('\033[1;30;42mYou win!\033[m')
+    print('🏆\033[1;30;42mYou win!\033[m')
 else:
-    print('\033[1;30;41mYou lose!\033[m')
+    print('💀\033[1;30;41mYou lose!\033[m')
 
 
 
